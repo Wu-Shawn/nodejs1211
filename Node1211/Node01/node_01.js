c47f27e2b7aae05e8fae1332dfdf54e9514aecb5
吴显毅 <15998413500@163.com>
2018-03-26 21:03:55 +0800

    【提交人】吴显毅 【提交时间】2018.03.26 【提交内容】作业

--- Node1211/Node01/node_01.js
+++ Node1211/Node01/node_01.js
@@ -1,1 +1,34 @@
 //git 测试
+console.log("hello node");
+
+
+{
+    var a = 10;
+    //let 作用域  只在当前代码段内有效
+    let b = 11;
+    console.log(b);
+}
+console.log(a);
+
+//let在循环中的应用
+var arr = [];
+for (let i = 0;i<10;i++){
+    arr[i] = function () {
+        console.log(i);
+    }
+}
+arr[6]();
+
+//let 没有变量提升
+
+//let 不允许重复定义
+
+
+function a1() {
+
+    function abc() {
+        let a123 = "hello world123";
+    }
+    console.log(a123);
+}
+a1()
